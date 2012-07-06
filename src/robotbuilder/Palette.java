8feab130a460fa7795eda0d103d77f19985b46a4
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-07-06 14:06:15 +0000

    Create the palette from the JSON description file.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@10 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- src/robotbuilder/Palette.java
+++ src/robotbuilder/Palette.java
@@ -2,12 +2,21 @@
 package robotbuilder;
 
 import java.awt.GridLayout;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Iterator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 
 /**
  *
@@ -16,28 +25,52 @@ import javax.swing.tree.DefaultMutableTreeNode;
 public class Palette extends JPanel {
     
     public Palette() {
+        FileReader file;
+        try {
+            file = new FileReader("/Users/brad/Dropbox/Projects/NetBeansProjects/RobotBuilder/src/robotbuilder/PaletteDescription.json");
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(Palette.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
+        JSONTokener tokener;
+        tokener = new JSONTokener(file);
+        JSONObject json;
+        try {
+            json = new JSONObject(tokener);
+        } catch (JSONException ex) {
+            Logger.getLogger(Palette.class.getName()).log(Level.SEVERE, null, ex);
+            return;
+        }
+        
         DefaultMutableTreeNode root = new DefaultMutableTreeNode("Palette");
-        DefaultMutableTreeNode sensorsNode = new DefaultMutableTreeNode("Sensors");
-        sensorsNode.add(new DefaultMutableTreeNode("Gyro"));
-        sensorsNode.add(new DefaultMutableTreeNode("Accelerometer"));
-        sensorsNode.add(new DefaultMutableTreeNode("Quadrature encoder"));
-        sensorsNode.add(new DefaultMutableTreeNode("Potentiometer"));
-        sensorsNode.add(new DefaultMutableTreeNode("LimitSwitch"));
-        DefaultMutableTreeNode motorsNode = new DefaultMutableTreeNode("Speed controllers");
-        motorsNode.add(new DefaultMutableTreeNode("Victor"));
-        motorsNode.add(new DefaultMutableTreeNode("Jaguar"));
-        DefaultMutableTreeNode OINode = new DefaultMutableTreeNode("Operator input");
-        OINode.add(new DefaultMutableTreeNode("Joystick analog"));
-        OINode.add(new DefaultMutableTreeNode("Joystick button"));
-        OINode.add(new DefaultMutableTreeNode("Extended I/O digital input"));
-        OINode.add(new DefaultMutableTreeNode("Extended I/O analog input"));
-        root.add(sensorsNode);
-        root.add(motorsNode);
-        root.add(OINode);
-
+        try {
+            createTree(root, json.getJSONObject("Palette"));
+        } catch (JSONException ex) {
+            Logger.getLogger(Palette.class.getName()).log(Level.SEVERE, null, ex);
+        }
         JTree paletteTree = new JTree(root);
         paletteTree.setDragEnabled(true);
         
         add(paletteTree);
      }
+
+    /**
+     * Build the palette tree recursively by traversing the JSON data object
+     * @param root The parent tree node
+     * @param jSONObject The JSON object that corresponds to this level
+     */
+    private void createTree(DefaultMutableTreeNode root, JSONObject jSONObject) {
+        for (Iterator i = jSONObject.keys(); i.hasNext(); ) {
+            String key = (String) i.next();
+            DefaultMutableTreeNode node = new DefaultMutableTreeNode(key);
+            root.add(node);
+            if (!jSONObject.isNull(key)) {
+                try {
+                    createTree(node, jSONObject.getJSONObject(key));
+                } catch (JSONException ex) {
+                    Logger.getLogger(Palette.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
 }
