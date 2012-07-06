369d43f51b6ca8d278d011e711736413c4c4f634
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-07-06 14:16:29 +0000

    Add OS look and feel.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@250 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- src/robotbuilder/RobotBuilder.java
+++ src/robotbuilder/RobotBuilder.java
@@ -3,7 +3,13 @@ package robotbuilder;
 
 import java.awt.EventQueue;
 import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JFrame;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -18,6 +24,22 @@ public class RobotBuilder {
             public void run() {
                 System.out.println(System.getProperty("java.class.path"));
                 System.out.println((new File(".")).getAbsolutePath());
+                System.out.println(UIManager.getSystemLookAndFeelClassName());
+                for (LookAndFeelInfo laf : UIManager.getInstalledLookAndFeels()) {
+                    System.out.println(laf);
+                }
+                try {
+                    UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(RobotBuilder.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (InstantiationException ex) {
+                    Logger.getLogger(RobotBuilder.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IllegalAccessException ex) {
+                    Logger.getLogger(RobotBuilder.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (UnsupportedLookAndFeelException ex) {
+                    Logger.getLogger(RobotBuilder.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 MainFrame frame = MainFrame.getInstance();
                 frame.openDefaultFile();
                 frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
