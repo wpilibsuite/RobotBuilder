3ea0d99ca387fad69655ee0c1b1e92e5638da7e0
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-07-06 14:05:57 +0000

    changed the UI slightly
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@3 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- src/robotbuilder/Palette.java
+++ src/robotbuilder/Palette.java
@@ -4,6 +4,11 @@
  */
 package robotbuilder;
 
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.GridLayout;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -13,6 +18,13 @@ import javax.swing.JPanel;
  */
 public class Palette extends JPanel {
     public Palette() {
-        add(new JLabel("Palette"));
+	setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+	JPanel grid = new JPanel(new GridLayout(2, 10));
+	add(grid);
+        grid.add(new JLabel("OneThing"));
+	grid.add(new JLabel("anotherThing"));
+	grid.add(new JLabel("Third thing"));
+	grid.add(new JLabel("Forth thing"));
+	add(Box.createVerticalGlue());
     }
 }
