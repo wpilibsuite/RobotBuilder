ee075101ac8e807f66b7168abf414f2a53fe33e9
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-07-06 14:12:22 +0000

    Implemented "Save As..."
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@159 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- src/robotbuilder/actions/SaveAsAction.java
+++ src/robotbuilder/actions/SaveAsAction.java
@@ -4,7 +4,9 @@ package robotbuilder.actions;
 import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.JFileChooser;
 import robotbuilder.ActionsClass;
+import robotbuilder.MainFrame;
 
 /**
  *
@@ -13,6 +15,7 @@ import robotbuilder.ActionsClass;
 public class SaveAsAction extends AbstractAction {
     ActionsClass saveAsAction;
 
+    JFileChooser fileChooser = new JFileChooser();
     public SaveAsAction() {
         putValue(Action.NAME, "Save as...");
         putValue(Action.SHORT_DESCRIPTION, "Save robot map to a new file");
@@ -21,6 +24,21 @@ public class SaveAsAction extends AbstractAction {
     @Override
     public void actionPerformed(ActionEvent ae) {
         System.out.println("Save as selected");
+        String filePath;
+        int result = fileChooser.showSaveDialog(MainFrame.getInstance().getFrame());
+        if (result == JFileChooser.CANCEL_OPTION) {
+            return;
+        }
+        else if (result == JFileChooser.ERROR_OPTION) {
+            return;
+        }
+        else if (result == JFileChooser.APPROVE_OPTION) {
+            filePath = fileChooser.getSelectedFile().getName();
+            if (!filePath.endsWith(".yaml"))
+                filePath += ".yaml";
+            
+            MainFrame.getInstance().getCurrentRobotTree().save(filePath);
+        }
     }
     
 }
