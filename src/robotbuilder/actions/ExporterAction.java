0b714891431e820fa343d124661fe416761a68be
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-08-20 15:24:22 +0000

    Added busy cursor (mostly).
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@384 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- src/robotbuilder/actions/ExporterAction.java
+++ src/robotbuilder/actions/ExporterAction.java
@@ -1,12 +1,15 @@
 
 package robotbuilder.actions;
 
+import java.awt.Cursor;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.util.Currency;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
+import javax.swing.UIManager;
 import robotbuilder.MainFrame;
 import robotbuilder.exporters.GenericExporter;
 
@@ -25,11 +28,13 @@ public class ExporterAction extends AbstractAction {
 
     @Override
     public void actionPerformed(ActionEvent ae) {
+        MainFrame.getInstance().setCursor(Cursor.WAIT_CURSOR);
         try {
             exporter.export(MainFrame.getInstance().getCurrentRobotTree());
         } catch (IOException ex) {
             Logger.getLogger(ExporterAction.class.getName()).log(Level.SEVERE, null, ex);
         }
+        MainFrame.getInstance().setCursor(Cursor.DEFAULT_CURSOR);
     }
     
     public boolean isOnToolbar() {
