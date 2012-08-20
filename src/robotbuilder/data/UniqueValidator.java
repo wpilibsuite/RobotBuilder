f2d321fb2ae1915babefbc360be2687d92393bd5
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-08-20 23:16:53 +0000

    Fixed NullPointer Issue
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@390 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- src/robotbuilder/data/UniqueValidator.java
+++ src/robotbuilder/data/UniqueValidator.java
@@ -46,6 +46,7 @@ public class UniqueValidator implements Validator {
     @Override
     public String getError(RobotComponent component, Property property) {
         Pair claimant = claims.get(getMap(component, getPrefix(property.getName())));
+        if (claimant != null) return null;
         return "This port is in use by "+claimant.toString()+" please change this to an unused port.";
     }
     
