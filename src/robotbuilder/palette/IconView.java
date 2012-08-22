0502717aca3dd97d1b70b53fea01b22a230ee978
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-08-22 21:03:12 +0000

    Initial working version of the icon view.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@408 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- src/robotbuilder/palette/IconView.java
+++ src/robotbuilder/palette/IconView.java
@@ -4,7 +4,24 @@
  */
 package robotbuilder.palette;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.plaf.BorderUIResource;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeModel;
+import robotbuilder.Utils;
+import robotbuilder.data.PaletteComponent;
 
 /**
  *
@@ -15,6 +32,33 @@ public class IconView extends JPanel {
     
     public IconView(Palette palette) {
         this.palette = palette;
+        
+        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        
+        TreeModel model = palette.getPaletteModel();
+        Enumeration children = ((DefaultMutableTreeNode) model.getRoot()).children();
+        while (children.hasMoreElements()) {
+            DefaultMutableTreeNode child = (DefaultMutableTreeNode) children.nextElement();
+            add(new IconSection(child));
+        }
+    }
+
+    private static class IconSection extends JPanel {
+        private IconSection(DefaultMutableTreeNode node) {
+            setBorder(BorderFactory.createTitledBorder(node.getUserObject().toString()));
+            setLayout(new GridLayout(0, 2));
+            
+            Enumeration children = node.children();
+            while (children.hasMoreElements()) {
+                PaletteComponent component = (PaletteComponent) ((DefaultMutableTreeNode) children.nextElement()).getUserObject();
+                add(new PaletteIcon(component));
+            }
+        }
     }
     
+    private static class PaletteIcon extends JLabel {
+        private PaletteIcon(PaletteComponent component) {
+            setIcon(new ImageIcon(Utils.getResource("/icons/"+component.getName()+".png")));
+        }
+    }
 }
