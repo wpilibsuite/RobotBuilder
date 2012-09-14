393cf57f3a033f124f371577424f3222dcb88792
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-09-14 21:05:51 +0000

    Added command groups to c++ export.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@446 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

---
+++ resources/export/cpp/CommandGroup.cpp
@@ -0,0 +1,23 @@
+
+#set($command = $helper.getByName($command-name, $robot))
+
+\#include "#class($command.name).h"
+
+#class($command.name)::#class($command.name)() {
+        // Add Commands here:
+        // e.g. AddSequential(new Command1());
+        //      AddSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use AddParallel()
+        // e.g. AddParallel(new Command1());
+        //      AddSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+}
---
+++ resources/export/cpp/CommandGroup.h
@@ -0,0 +1,19 @@
+
+#set($command = $helper.getByName($command-name, $robot))
+
+#ifndef #constant($command.name)_H
+\#define #constant($command.name)_H
+
+\#include "Commands/CommandGroup.h"
+
+/**
+ *
+ *
+ * @author ExampleAuthor
+ */
+class #class($command.name): public CommandGroup {
+public:	
+	#class($command.name)();
+};
+
+#endif
