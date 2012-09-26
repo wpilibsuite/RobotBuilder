4fcfdd4403ec531e3a0aa6477ccf28eb331a19e7
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-09-26 23:02:47 +0000

    Fixed PID Subsystem stuff.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@495 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- resources/export/cpp/PIDSubsystem-pid.cpp
+++ resources/export/cpp/PIDSubsystem-pid.cpp
@@ -1,9 +1,9 @@
 #class($subsystem.name)::#class($subsystem.name)() : PIDSubsystem("#class($subsystem.name)", ${subsystem.getProperty("P").getValue()}, ${subsystem.getProperty("I").getValue()}, ${subsystem.getProperty("D").getValue()}) {
 #set($subsystem = $helper.getByName($subsystem-name, $robot))
-        getPIDController().setContinuous(${subsystem.getProperty("Continuous").getValue()});
+        getPIDController()->SetContinuous(${subsystem.getProperty("Continuous").getValue()});
 #if($subsystem.getProperty("Limit Input").getValue())
-        getPIDController().setInputRange(${subsystem.getProperty("Minimum Input").getValue()}, ${subsystem.getProperty("Maximum Input").getValue()});
+        getPIDController()->SetInputRange(${subsystem.getProperty("Minimum Input").getValue()}, ${subsystem.getProperty("Maximum Input").getValue()});
 #end
 #if($subsystem.getProperty("Limit Output").getValue())
-        getPIDController().setOutputRange(${subsystem.getProperty("Minimum Output").getValue()}, ${subsystem.getProperty("Maximum Output").getValue()});
+        getPIDController()->SetOutputRange(${subsystem.getProperty("Minimum Output").getValue()}, ${subsystem.getProperty("Maximum Output").getValue()});
 #end
