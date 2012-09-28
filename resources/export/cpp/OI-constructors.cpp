66d0072470bde74bf7907f033ec50d76c290f14c
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-09-28 20:53:06 +0000

    Fixed excessive newlines in OI.cpp
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@512 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- resources/export/cpp/OI-constructors.cpp
+++ resources/export/cpp/OI-constructors.cpp
@@ -1,9 +1,11 @@
 ${Collections.reverse($components)}
 #foreach( $component in $components )
 #if ($helper.exportsTo("OI", $component))
+#if ("#declaration($component)" != "")
 	#constructor($component)
 
 	#extra($component)
 
+#end
 #end
 #end
--- resources/export/cpp/OI-declarations.h
+++ resources/export/cpp/OI-declarations.h
@@ -1,6 +1,8 @@
 #foreach ($component in $components)
 #if ($helper.exportsTo("OI", $component))
+#if ("#declaration($component)" != "")
 	#declaration($component)
 
+#end
 #end
 #end
