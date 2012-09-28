5d2fe15b085d8b46b244c6fa657844ee09ba2753
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-09-28 18:11:03 +0000

    Fixed robotmap.cpp export.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@506 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- resources/export/cpp/RobotMap.cpp
+++ resources/export/cpp/RobotMap.cpp
@@ -2,6 +2,12 @@
 \#include "RobotMap.h"
 ${helper.getImports($robot, "RobotMap")}
 
+#foreach ($component in $components)
+#if ($helper.exportsTo("RobotMap", $component))
+#type($component)* RobotMap::#variable($component.name) = 0;
+#end
+#end
+
 void RobotMap::init() {
 #foreach ($component in $components)
 #if ($helper.exportsTo("RobotMap", $component))
