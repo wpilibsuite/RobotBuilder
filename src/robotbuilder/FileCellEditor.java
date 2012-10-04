9a581007af7b0a6d287da4cd49d2355cc4af9d89
mwills <mwills@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-10-04 21:15:24 +0000

    Fixed FileProperty FileChooser
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@514 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- src/robotbuilder/FileCellEditor.java
+++ src/robotbuilder/FileCellEditor.java
@@ -5,8 +5,15 @@
 package robotbuilder;
 
 import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.Objects;
 import javax.swing.AbstractCellEditor;
+import javax.swing.JButton;
 import javax.swing.JFileChooser;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.TableCellEditor;
 
@@ -14,21 +21,29 @@ import javax.swing.table.TableCellEditor;
  *
  * @author Alex Henning
  */
-public class FileCellEditor extends AbstractCellEditor implements TableCellEditor {
+public class FileCellEditor extends AbstractCellEditor implements TableCellEditor, MouseListener {
     JFileChooser fileChooser;
-
+    JLabel button;
+    JTable table;
     
     public FileCellEditor(JFileChooser fileChooser) {
-        this.fileChooser = fileChooser;
+        this.fileChooser = new JFileChooser();
+        button = new JLabel();
+        button.addMouseListener(this);
     }
     
     @Override
     public Component getTableCellEditorComponent(JTable table,
             Object value, boolean isSelected, int row, int column) {
-   
-            fileChooser.showSaveDialog(table);
-
-        return null;
+        this.table = table;
+        button.setText(value.toString());
+        JFileChooser valueChooser = ((JFileChooser)value);
+        fileChooser.setSelectedFile(valueChooser.getSelectedFile());
+        fileChooser.setFileSelectionMode(valueChooser.getFileSelectionMode());
+        fileChooser.setApproveButtonText(valueChooser.getApproveButtonText());
+        fileChooser.setDialogTitle(valueChooser.getDialogTitle());
+        fileChooser.setFileFilter(valueChooser.getFileFilter());
+        return button;
     }
 
     @Override
@@ -38,6 +53,28 @@ public class FileCellEditor extends AbstractCellEditor implements TableCellEdito
         } catch (NullPointerException e) {
             return "";
         }
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        fileChooser.showSaveDialog(table);
+        fireEditingStopped();
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
     }
     
 }
