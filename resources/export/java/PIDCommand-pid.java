36bf3ef8fac055c5a6a2656bed29c353df9adf17
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-11-01 21:16:56 +0000

    Addede tolerance to PID Commands and PID Controllers.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@560 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- resources/export/java/PIDCommand-pid.java
+++ resources/export/java/PIDCommand-pid.java
@@ -1,6 +1,7 @@
 #set($command = $helper.getByName($command-name, $robot))
         super("#class($command-name)", ${command.getProperty("P").getValue()}, ${command.getProperty("I").getValue()}, ${command.getProperty("D").getValue()}, ${command.getProperty("Period").getValue()});
         getPIDController().setContinuous(${command.getProperty("Continuous").getValue()});
+        getPIDController().setAbsoluteTolerance(${command.getProperty("Tolerance").getValue()});
 #if($command.getProperty("Limit Input").getValue())
         getPIDController().setInputRange(${command.getProperty("Minimum Input").getValue()}, ${command.getProperty("Maximum Input").getValue()});
 #end
