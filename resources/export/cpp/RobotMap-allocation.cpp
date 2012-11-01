a3db4bc21886ff55c2fd2c85e86eeba770d5f0e8
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-11-01 21:17:11 +0000

    Used NULL instead of 0's for c++ pointers in export.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@561 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- resources/export/cpp/RobotMap-allocation.cpp
+++ resources/export/cpp/RobotMap-allocation.cpp
@@ -1,5 +1,5 @@
 #foreach ($component in $components)
 #if ($helper.exportsTo("RobotMap", $component))
-#type($component)* RobotMap::#constant($component.fullName) = 0;
+#type($component)* RobotMap::#constant($component.fullName) = NULL;
 #end
 #end
