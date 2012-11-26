d7b03179662f7608f23f4a9a2ab6cb719ca40eb7
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-11-26 21:15:42 +0000

    Removed all caps names from Java robotmap.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@563 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- resources/export/java/PIDCommand-source.java
+++ resources/export/java/PIDCommand-source.java
@@ -1,5 +1,5 @@
 #set($command = $helper.getByName($command-name, $robot))
 #set($component = $command.getProperty("Input").getValue())
 #set($name = ${helper.getByName($component, $robot).fullName})
-#if($name)        return RobotMap.#constant($name).pidGet();
+#if($name)        return RobotMap.#variable($name).pidGet();
 #end
