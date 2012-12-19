fde522eb218d19aa8d305816056efbb865037367
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2012-12-19 18:01:27 +0000

    Fixed bug in C++ template.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@606 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- resources/export/cpp/CommandBasedRobot-includes.h
+++ resources/export/cpp/CommandBasedRobot-includes.h
@@ -1,3 +1,4 @@
 #set($autonomous = $robot.getProperty("Autonomous Command").getValue())
 #if($autonomous != "None")\#include "Commands/#class($autonomous).h"
+#end
 ${helper.getImports($robot, "Robot")}
