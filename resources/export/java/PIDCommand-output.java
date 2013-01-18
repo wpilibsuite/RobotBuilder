f9481082ca5ad50d52409528dc86439e4b8fda92
alexhenning <alexhenning@7d5bc05a-d525-4a0d-8ff2-4902128d4f3b>
2013-01-18 16:04:21 +0000

    Fixed artf1590.
    git-svn-id: http://firstforge.wpi.edu/svn/repos/robotbuilder/trunk@619 7d5bc05a-d525-4a0d-8ff2-4902128d4f3b

--- resources/export/java/PIDCommand-output.java
+++ resources/export/java/PIDCommand-output.java
@@ -1,5 +1,5 @@
 #set($command = $helper.getByName($command-name, $robot))
 #set($component = $command.getProperty("Output").getValue())
 #set($name = ${helper.getByName($component, $robot).fullName})
-#if($name)        RobotMap.#constant($name).pidWrite(output);
+#if($name)        RobotMap.#variable($name).pidWrite(output);
 #end
