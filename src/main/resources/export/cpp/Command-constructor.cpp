3c6423ee975de5cd24ca4c26f8ad0c83e45a9837
Patrick Hurley <phurley@gmail.com>
2016-12-08 00:35:05 -0500

    Allow command extensions to use a custom base class.  (#11)
    * Allow command extensions to use a custom base class. Tested using a java
    project, and the generated code looks acceptable in C++, but not tested
    on an actual robot.
    
    * Handle C++ constructor with custom base class in code generator

--- src/main/resources/export/cpp/Command-constructor.cpp
+++ src/main/resources/export/cpp/Command-constructor.cpp
@@ -2,11 +2,12 @@
 #set($params = $command.getProperty("Parameters").getValue())
 #set($len = $params.size() - 2)
 #set($last = $len + 1)
+#macro( klass $cmd )#if( "#type($cmd)" == "" )Command#else#type($cmd)#end#end
 
 #if( $params.size() > 0 )
-#class($command.name)::#class($command.name)(#if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_cpp($params.get($i)), #end#end#if( $last >= 0 )#param_declaration_cpp($params.get($last))#end): Command() {
+#class($command.name)::#class($command.name)(#if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_cpp($params.get($i)), #end#end#if( $last >= 0 )#param_declaration_cpp($params.get($last))#end): #klass($command)() {
 #else
-#class($command.name)::#class($command.name)(): Command() {
+#class($command.name)::#class($command.name)(): #klass($command)() {
 #end
     #foreach($param in $params)
 m_$param.getName() = $param.getName();
