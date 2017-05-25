b799c0bf3765337cb1862bb764cfafb02f5af497
sciencewhiz <git@rossesmail.com>
2017-05-24 20:50:57 -0700

    Fix comment about updating class or package
    Fixes #42

--- src/main/resources/export/java/RobotTemplate.java
+++ src/main/resources/export/java/RobotTemplate.java
@@ -13,8 +13,8 @@ import ${package}.subsystems.*;
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
  * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
+ * creating this project, you must also update the build.properties file in 
+ * the project.
  */
 public class Robot extends IterativeRobot {
 
