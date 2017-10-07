fae14f01a3169e2e018f0d3de5fa7e574f8f237b
sciencewhiz <git@rossesmail.com>
2017-10-06 19:17:52 -0700

    Add override syntax to generated code.
    Fixes #37

--- src/main/resources/export/java/Command.java
+++ src/main/resources/export/java/Command.java
@@ -28,24 +28,29 @@ public class #class($command.name) extends #klass($command) {
     }
 
     // Called just before this Command runs the first time
+    @Override
     protected void initialize() {
     }
 
     // Called repeatedly when this Command is scheduled to run
+    @Override
     protected void execute() {
     }
 
     // Make this return true when this Command no longer needs to run execute()
+    @Override
     protected boolean isFinished() {
         return false;
     }
 
     // Called once after isFinished returns true
+    @Override
     protected void end() {
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
+    @Override
     protected void interrupted() {
     }
 }
