7886a292d86e8eebf08313da2177e0ad5d641e02
sciencewhiz <sciencewhiz@users.noreply.github.com>
2017-10-16 20:54:29 -0700

    Add option to put Subsystems and PIDControllers to SmartDashboard (#54)
    Fixes #35

--- src/main/resources/export/java/RobotTemplate-constructors.java
+++ src/main/resources/export/java/RobotTemplate-constructors.java
@@ -2,5 +2,8 @@
 #if ($helper.exportsTo("Robot", $component))
         #constructor($component)
 
+#if ($component.getProperty("Send to SmartDashboard").getValue())
+        SmartDashboard.putData(#variable($component.name));
+#end
 #end
 #end
