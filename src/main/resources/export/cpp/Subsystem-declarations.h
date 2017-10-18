4513f65536c10b5543c14bae46ae4c6ac46c5e72
sciencewhiz <git@rossesmail.com>
2017-10-17 22:57:54 -0700

    Don't prefix ClassName in templates, add frc:: to ClassName in export description

--- src/main/resources/export/cpp/Subsystem-declarations.h
+++ src/main/resources/export/cpp/Subsystem-declarations.h
@@ -1,6 +1,6 @@
 #set($subsystem = $helper.getByName($subsystem-name, $robot))
 #foreach ($component in $components)
 #if ($component.subsystem == $subsystem.subsystem && $component != $subsystem)
-	std::shared_ptr<frc::#type($component)> #variable($component.name);
+	std::shared_ptr<#type($component)> #variable($component.name);
 #end
 #end
