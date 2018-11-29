3eab9f806fff53d91d28436fd5e6095157929568
Nicolas Machado <nxmq08@gmail.com>
2018-11-29 18:21:22 -0500

    Fix PIDSubsystem source/sink generation (#118)
    Fixes #117

--- src/main/resources/export/java/PIDCommand-source.java
+++ src/main/resources/export/java/PIDCommand-source.java
@@ -2,7 +2,7 @@
 #set($subsystem = $command.getProperty("Requires").getValue())
 #set($subsystem-instance = $helper.getByName($subsystem,$robot))
 #foreach ($component in $components)
-#if ($component.name == $command.getProperty("Input").getValue())
+#if ($component.name == $command.getProperty("Input").getValue() && $component.getSubsystem().equals($subsystem.concat(" ")))
         return Robot.#variable($subsystem).get#class($component.name)().pidGet();
 #end
 #end
--- src/main/resources/export/java/PIDSubsystem-output.java
+++ src/main/resources/export/java/PIDSubsystem-output.java
@@ -1,6 +1,6 @@
 #set($subsystem = $helper.getByName($subsystem-name, $robot))
 #foreach ($component in $components)
-#if ($component.name == $subsystem.getProperty("Output").getValue())
-    #variable($component.name).pidWrite(output);
+#if ($component.name == $subsystem.getProperty("Output").getValue() && $component.getSubsystem().equals($subsystem-name.concat(" ")))
+        #variable($component.name).pidWrite(output);
 #end
 #end
