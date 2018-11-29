2260bdaebe77f1c582fc101695689568f5d3a66b
Nicolas Machado <nxmachado@wpi.edu>
2018-11-29 17:41:01 -0500

    Fix issue #117

--- src/main/resources/export/java/PIDSubsystem-source.java
+++ src/main/resources/export/java/PIDSubsystem-source.java
@@ -1,6 +1,6 @@
 #set($subsystem = $helper.getByName($subsystem-name, $robot))
 #foreach ($component in $components)
-#if ($component.name == $subsystem.getProperty("Input").getValue())
-    return #pid($component);
+#if ($component.name == $subsystem.getProperty("Input").getValue() && $component.getSubsystem().equals($subsystem-name.concat(" ")))
+        return #pid($component);
 #end
 #end
