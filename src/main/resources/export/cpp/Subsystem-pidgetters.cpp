cff925cce464fcf92c52c1e450906d1b229671af
sciencewhiz <sciencewhiz@users.noreply.github.com>
2019-02-14 22:54:31 -0800

    Fix export of the pid getters in C++ (#137)

--- src/main/resources/export/cpp/Subsystem-pidgetters.cpp
+++ src/main/resources/export/cpp/Subsystem-pidgetters.cpp
@@ -1,11 +1,11 @@
 #foreach ($component in $components)
 #if ($component.getBase().getType() == "Command" && $subsystem-name == $component.getProperty("Requires").getValue() && $component.getProperty("Input").getValue())
-        std::shared_ptr<frc::PIDSource> #class($subsystem-name)::Get#class($component.getProperty("Input").getValue())() {
-            return #variable($component.getProperty("Input").getValue());
-        }
+std::shared_ptr<frc::PIDSource> #class($subsystem-name)::Get#class($component.getProperty("Input").getValue())() {
+    return #variable($component.getProperty("Input").getValue());
+}
 
-         std::shared_ptr<frc::PIDOutput> #class($subsystem-name)::Get#class($component.getProperty("Output").getValue())() {
-            return #variable($component.getProperty("Output").getValue());
-        }
+std::shared_ptr<frc::PIDOutput> #class($subsystem-name)::Get#class($component.getProperty("Output").getValue())() {
+    return #variable($component.getProperty("Output").getValue());
+}
 #end
 #end
