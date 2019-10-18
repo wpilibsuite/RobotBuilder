e8ce49edb41b785d86a4b179acae8d354b537389
Corey Applegate <cpapplefamily@gmail.com>
2019-10-18 11:39:41 -0500

    Added Buttons for RobotContainer

--- src/main/resources/export/java/RobotContainer-declarations.java
+++ src/main/resources/export/java/RobotContainer-declarations.java
@@ -1,6 +1,19 @@
+// The robot's subsystems
 #foreach ($component in $components)
-#if ($helper.exportsTo("RobotContainer", $component))
+#if ($helper.exportsTo("RobotContainer", $component)
+&& "#type($component)" != "" 
+&& "#type($component)" == "SubsystemBase")
     #declaration($component)
+
+#end
+#end
+
+// Joysticks
+#foreach ($component in $components)
+#if ($helper.exportsTo("OI", $component)
+     && "#type($component)" != "" 
+     && "#type($component)" == "Joystick")
+    #constructor($component)
 
 #end
 #end
