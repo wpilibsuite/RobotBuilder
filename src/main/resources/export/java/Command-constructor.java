e2b4500f69c6bc1df629d06648325eb6aaefb600
Corey Applegate <corey.applegate@electrolux.com>
2019-10-19 12:18:17 -0500

    Set Command Sub Requirements

--- src/main/resources/export/java/Command-constructor.java
+++ src/main/resources/export/java/Command-constructor.java
@@ -2,11 +2,21 @@
 #set($params = $command.getProperty("Parameters").getValue())
 #set($len = $params.size() - 2)
 #set($last = $len + 1)
+
+#if ($command.getProperty("Requires").getValue() != "None")
+
 #if( $params.size() > 0 )
-    public #class($command.name)(#if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_java($params.get($i)), #end#end#param_declaration_java($params.get($last))) {
+    public #class($command.name)(#class($command.getProperty("Requires").getValue()) subsystem, #if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_java($params.get($i)), #end#end#param_declaration_java($params.get($last))) {
+#else
+    public #class($command.name)(#class($command.getProperty("Requires").getValue()) subsystem) {
+
+#end
+
 #else
     public #class($command.name)() {
+
 #end
+
 #if ( $command.getProperty("Run When Disabled").getValue() )
         setRunWhenDisabled(true);
 #end
