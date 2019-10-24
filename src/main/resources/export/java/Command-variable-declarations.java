52adb6ee0b61f00bd952d7f20283edcd4b959a35
Corey Applegate <corey.applegate@electrolux.com>
2019-10-24 15:01:16 -0500

    Fix for many of the re-generate RB

--- src/main/resources/export/java/Command-variable-declarations.java
+++ src/main/resources/export/java/Command-variable-declarations.java
@@ -1,5 +1,5 @@
 #if ($command.getProperty("Requires").getValue() != "None")
-private final #class($command.getProperty("Requires").getValue()) m_#variable($command.getProperty("Requires").getValue());
+        private final #class($command.getProperty("Requires").getValue()) m_#variable($command.getProperty("Requires").getValue());
 #end
 #set($command = $helper.getByName($command_name, $robot))
 #set($params = $command.getProperty("Parameters").getValue())
