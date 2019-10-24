e608a7ffa2307f86d9e79a713a29e1f2f19d00a6
Joe <sciencewhiz@users.noreply.github.com>
2019-10-24 10:29:52 -0700

    Fix export of PIDCommand

--- src/main/resources/export/java/PIDCommand-source.java
+++ src/main/resources/export/java/PIDCommand-source.java
@@ -1,6 +1,6 @@
 #set($command = $helper.getByName($command_name, $robot))
 #set($subsystem = $command.getProperty("Requires").getValue())
-#set($subsystem-instance = $helper.getByName($subsystem,$robot))
+#set($subsystem_instance = $helper.getByName($subsystem,$robot))
 #foreach ($component in $components)
 #if ($component.name == $command.getProperty("Input").getValue() && $component.getSubsystem().equals($subsystem.concat(" ")))
         return Robot.#variable($subsystem).get#class($component.name)().pidGet();
