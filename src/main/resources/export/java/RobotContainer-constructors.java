718abb4725eee89fd6624b887af1d594ff20a090
Corey Applegate <cpapplefamily@gmail.com>
2019-10-27 14:40:41 -0500

    WIthTimeOut added to Commands and buttons(Priorety)

--- src/main/resources/export/java/RobotContainer-constructors.java
+++ src/main/resources/export/java/RobotContainer-constructors.java
@@ -8,7 +8,8 @@
         #foreach ($command in $commands)
                 #if($command.name == $component.getProperty("Command").value)
                         #set($params = $component.getProperty("Parameters").getValue())
-                        #variable($component.name).$component.getProperty("When to Run").getValue()(#new_command_instantiation($component,$command,$params));
+                        #variable($component.name).$component.getProperty("When to Run").getValue()(#new_command_instantiation($component,$command,$params)#if($component.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value));#else#if($command.getProperty("Add Timeout").value == true).withTimeout($command.getProperty("Timeout").value));#else);#end#end
+                        
                 #end
         #end
         
@@ -23,22 +24,22 @@
                 //No Parameters
                 #if ($component.getProperty("Requires").getValue() != "None")
                         //Does Require Subsystem
-                        SmartDashboard.putData("$component.getName()", new #class($component.getName())(m_#required_subsystem($component)));
+                        SmartDashboard.putData("$component.getName()", new #class($component.getName())(m_#required_subsystem($component))#if($component.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value)#end);
                 #else
                         //Does Not Require Subsystem
-                        SmartDashboard.putData("$component.getName()", new #class($component.getName())());
+                        SmartDashboard.putData("$component.getName()", new #class($component.getName())()#if($component.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value)#end);
                 #end
         #else
                 //Has Parameters
                 #if ($component.getProperty("Requires").getValue() != "None")
                         //Does Require Subsystem
                         #foreach( $set in $component.getProperty("Parameter presets").getValue() )
-                                SmartDashboard.putData("$component.getName(): $set.getName()", new #class($component.getName())(#command_params($set.getParameters()), m_#required_subsystem($component)));
+                                SmartDashboard.putData("$component.getName(): $set.getName()", new #class($component.getName())(#command_params($set.getParameters()), m_#required_subsystem($component))#if($component.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value)#end);
                         #end
                 #else
                         //Does Not Require Subsystem
                         #foreach( $set in $component.getProperty("Parameter presets").getValue() )
-                                SmartDashboard.putData("$component.getName(): $set.getName()", new #class($component.getName())(#command_params($set.getParameters())));
+                                SmartDashboard.putData("$component.getName(): $set.getName()", new #class($component.getName())(#command_params($set.getParameters()))#if($component.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value)#end);
                         #end
                 #end
         #end
