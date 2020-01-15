e0f275e0baaf212c0b77d78891b7d8b021b57f67
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-01-14 20:58:25 -0800

    Update templates based on latest VS Code templates (#196)

--- src/main/resources/export/java/RobotTemplate-constructors.java
+++
@@ -1,9 +0,0 @@
-#foreach ($component in $components)
-#if ($helper.exportsTo("Robot", $component))
-        #constructor($component)
-
-#if ($component.getProperty("Send to SmartDashboard").getValue())
-        SmartDashboard.putData(#variable($component.name));
-#end
-#end
-#end
