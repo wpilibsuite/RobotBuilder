deeab8ad7d53c4c30c4dc131385789dfe5f61e4c
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-01-15 22:02:28 -0800

    Update to export to 2020.2.1 (#199)

--- src/main/java/robotbuilder/exporters/GenericExporter.java
+++ src/main/java/robotbuilder/exporters/GenericExporter.java
@@ -158,7 +158,7 @@ public class GenericExporter {
             }
         }
         if(wpilibRelease == null) {
-            wpilibRelease = "2020.1.2"; // this shouldn't need to be relied upon,
+            wpilibRelease = "2020.2.1"; // this shouldn't need to be relied upon,
                                         // but its better than generating nothing.
         }
 
