f353e85bbba28ee8510c6bb036739268b625e75b
Joe <sciencewhiz@users.noreply.github.com>
2020-05-25 21:15:54 -0700

    Improve command generation
    Only allow one timeout
    Remove duplicate code for generating a command for JoystickButton and
    SmartDashboard
    Fix parameters when parameter preset not used

--- src/main/resources/export/java/RobotContainer-constructors.java
+++ src/main/resources/export/java/RobotContainer-constructors.java
@@ -8,7 +8,8 @@
         #foreach ($command in $commands)
                 #if($command.name == $component.getProperty("Command").value)
                         #set($params = $component.getProperty("Parameters").getValue())
-                        #variable($component.name).$component.getProperty("When to Run").getValue()(#new_command_instantiation($component,$command,$params)#if($component.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value));#else#if($command.getProperty("Add Timeout").value == true).withTimeout($command.getProperty("Timeout").value));#else);#end#end
+                        #variable($component.name).$component.getProperty("When to Run").getValue()(#new_command_instantiation($component,$command,$params),$component.getProperty("Interruptible").getValue());
+                        SmartDashboard.putData("$component.getName()",#new_command_instantiation($component,$command,$params));
                         
                 #end
         #end
@@ -16,33 +17,4 @@
 #end
 #end
 
-        // SmartDashboard Buttons
-#foreach( $component in $components )
-#if ($component.getBase().getType() == "Command"
-     && $component.getProperty("Button on SmartDashboard").getValue())
-        #if( $component.getProperty("Parameter presets").getValue().isEmpty() )
-                //No Parameters
-                #if ($component.getProperty("Requires").getValue() != "None")
-                        //Does Require Subsystem
-                        SmartDashboard.putData("$component.getName()", new #class($component.getName())(m_#required_subsystem($component))#if($component.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value)#end);
-                #else
-                        //Does Not Require Subsystem
-                        SmartDashboard.putData("$component.getName()", new #class($component.getName())()#if($component.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value)#end);
-                #end
-        #else
-                //Has Parameters
-                #if ($component.getProperty("Requires").getValue() != "None")
-                        //Does Require Subsystem
-                        #foreach( $set in $component.getProperty("Parameter presets").getValue() )
-                                SmartDashboard.putData("$component.getName(): $set.getName()", new #class($component.getName())(#command_params($set.getParameters()), m_#required_subsystem($component))#if($component.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value)#end);
-                        #end
-                #else
-                        //Does Not Require Subsystem
-                        #foreach( $set in $component.getProperty("Parameter presets").getValue() )
-                                SmartDashboard.putData("$component.getName(): $set.getName()", new #class($component.getName())(#command_params($set.getParameters()))#if($component.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value)#end);
-                        #end
-                #end
-        #end
-#end
-#end
 
