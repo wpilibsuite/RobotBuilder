73e4e8c14b1362e74d774fb3d2c3c1d6170d2105
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-06-13 16:04:32 -0700

    Implement Conditional Command for Java (#220)

--- src/main/resources/export/java/ConditionalCommand-constructors.java
+++ src/main/resources/export/java/ConditionalCommand-constructors.java
@@ -1,8 +1,24 @@
 #set($command = $helper.getByName($command_name, $robot))
-#set($onTrue = $command.getProperty("On True Command").getValue())
-#set($onFalse = $command.getProperty("On False Command").getValue())
-    public #class($command.name)() {
-      super(new #class($onTrue)(), new #class($onFalse)());
-#if ( $command.getProperty("Run When Disabled").getValue() )
-      setRunWhenDisabled(true);
+#set($onTrue = $helper.getByName($command.getProperty("On True Command").getValue(), $robot))
+#set($onFalse = $helper.getByName($command.getProperty("On False Command").getValue(), $robot))
+#set($params = $command.getProperty("Parameters").getValue())
+#set($len = $params.size() - 2)
+#set($last = $len + 1)
+
+#if ($command.getProperty("Requires").getValue() != "None")
+
+#if( $params.size() > 0 )
+    public #class($command.name)(#if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_java($params.get($i)), #end#end#param_declaration_java($params.get($last)), #class($command.getProperty("Requires").getValue()) subsystem) {
+#else
+    public #class($command.name)(#class($command.getProperty("Requires").getValue()) subsystem) {
+
 #end
+#else
+#if( $params.size() > 0 )
+    public #class($command.name)(#if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_java($params.get($i)), #end#end#param_declaration_java($params.get($last))) {
+
+#else
+    public #class($command.name)() {
+#end
+#end
+      super(#new_command_instantiation_cc($onTrue), #new_command_instantiation_cc($onFalse), () ->
