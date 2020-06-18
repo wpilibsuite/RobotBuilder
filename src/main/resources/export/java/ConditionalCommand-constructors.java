c0892d8ac5dee7c4e27201c62b156686fd75ce0e
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-06-17 20:56:58 -0700

    Clean up commands instantiation (#223)
    Fix default command and autonomous options command instantiation.
    Remove duplicate code between CondtionalCommand and Command
    Fix RobotContainer export

--- src/main/resources/export/java/ConditionalCommand-constructors.java
+++ src/main/resources/export/java/ConditionalCommand-constructors.java
@@ -1,24 +1,5 @@
-#set($command = $helper.getByName($command_name, $robot))
 #set($onTrue = $helper.getByName($command.getProperty("On True Command").getValue(), $robot))
 #set($onFalse = $helper.getByName($command.getProperty("On False Command").getValue(), $robot))
-#set($params = $command.getProperty("Parameters").getValue())
-#set($len = $params.size() - 2)
-#set($last = $len + 1)
 
-#if ($command.getProperty("Requires").getValue() != "None")
-
-#if( $params.size() > 0 )
-    public #class($command.name)(#if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_java($params.get($i)), #end#end#param_declaration_java($params.get($last)), #class($command.getProperty("Requires").getValue()) subsystem) {
-#else
-    public #class($command.name)(#class($command.getProperty("Requires").getValue()) subsystem) {
-
-#end
-#else
-#if( $params.size() > 0 )
-    public #class($command.name)(#if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_java($params.get($i)), #end#end#param_declaration_java($params.get($last))) {
-
-#else
-    public #class($command.name)() {
-#end
-#end
+#parse("${exporter_path}Command-constructors.java")
       super(#new_command_instantiation_cc($onTrue), #new_command_instantiation_cc($onFalse), () ->
