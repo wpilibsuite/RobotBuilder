311bb9e5cd4462afa03331ce85ff8a46d9f8e53c
Joe <sciencewhiz@users.noreply.github.com>
2020-06-22 21:39:59 -0700

    Use kF

--- src/main/resources/export/java/PIDSubsystem-output.java
+++ src/main/resources/export/java/PIDSubsystem-output.java
@@ -1,6 +1,6 @@
 #set($subsystem = $helper.getByName($subsystem_name, $robot))
 #foreach ($component in $components)
 #if ($component.name == $subsystem.getProperty("Output").getValue() && $component.getSubsystem().equals($subsystem_name.concat(" ")))
-        #variable($component.name).pidWrite(output);
+        #variable($component.name).pidWrite(output + kF*setpoint);
 #end
 #end
