b49a7f33b6c5736c870d814d9f762ecf7733a014
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-07-08 20:34:37 -0700

    Fix export of PIDSubsystem (#225)

--- src/main/resources/export/java/PIDSubsystem-source.java
+++ src/main/resources/export/java/PIDSubsystem-source.java
@@ -1,6 +1,10 @@
 #set($subsystem = $helper.getByName($subsystem_name, $robot))
 #foreach ($component in $components)
-#if ($component.name == $subsystem.getProperty("Input").getValue() && $component.getSubsystem().equals($subsystem_name.concat(" ")))
+    #if ($component.name == $subsystem.getProperty("Input").getValue() && $component.getSubsystem().equals($subsystem_name.concat(" ")))
+        #if($subsystem.getProperty("Limit Input").getValue() && !${subsystem.getProperty("Continuous").getValue()} )
+        return MathUtil.clamp(#pid($component), ${subsystem.getProperty("Minimum Input").getValue()}, ${subsystem.getProperty("Maximum Input").getValue()});
+        #else
         return #pid($component);
-#end
+        #end
+    #end
 #end
