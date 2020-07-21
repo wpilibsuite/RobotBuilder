745bf939141a5090e3337f57fd6f7e8ce75401f1
cbennet <cbennet@wpi.edu>
2020-07-21 14:16:58 -0400

    pid subsystem working

--- src/main/resources/export/cpp/PIDSubsystem-output.cpp
+++ src/main/resources/export/cpp/PIDSubsystem-output.cpp
@@ -1,6 +1,6 @@
 #set($subsystem = $helper.getByName($subsystem_name, $robot))
 #foreach ($component in $components)
 #if ($component.name == $subsystem.getProperty("Output").getValue())
-    #variable($component.name)->PIDWrite(output);
+    m_#variable($component.name).PIDWrite(output);
 #end
 #end
