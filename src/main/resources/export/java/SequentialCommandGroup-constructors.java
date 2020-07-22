89a0d36ebf0b79024dd3a702bdc52bded02805e1
Joe <sciencewhiz@users.noreply.github.com>
2020-07-21 20:08:32 -0700

    Fix export of SequentialCommandGroup

--- src/main/resources/export/java/SequentialCommandGroup-constructors.java
+++ src/main/resources/export/java/SequentialCommandGroup-constructors.java
@@ -3,10 +3,9 @@
 #set($len = $params.size() - 2)
 #set($last = $len + 1)
 
-public #class($command.name)() {
-###foreach ($command in $commands)
-##        #if ($command.getProperty("Requires").getValue() != "None")
- ##               #class($command.getProperty("Requires").getValue()) 
- ##               //Claw claw
- ##       #end
-###end) {
+#if( $params.size() > 0 )
+    public #class($command.name)(#if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_java($params.get($i)), #end#end#param_declaration_java($params.get($last))) {
+
+#else
+    public #class($command.name)() {
+#end
