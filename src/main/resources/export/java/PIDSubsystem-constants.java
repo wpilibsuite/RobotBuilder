e2485e34e654fcc8cfc16066cc4b98b4838cab69
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-07-27 19:38:51 -0700

    Fix modifications not being applied (#242)
    Add missing files to files.yaml
    Remove duplication

--- src/main/resources/export/java/PIDSubsystem-constants.java
+++
@@ -1,5 +0,0 @@
-#set( $subsystem = $helper.getByName($subsystem_name, $robot) )
-#set( $constants = $subsystem.getProperty("Constants").getValue())
-#foreach( $constant in $constants)
-    #declare_constant( $constant )
-#end
