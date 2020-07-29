cdaee7756352448c9cf72b26a4837c05c39c36e1
Joe <sciencewhiz@users.noreply.github.com>
2020-07-28 20:56:48 -0700

    Remove CommandGroup and TimedComman
    Fixes #240

--- src/main/resources/export/java/CommandGroup-declarations.java
+++
@@ -1,5 +0,0 @@
-#set($command = $helper.getByName($command_name, $robot))
-#set($commandDescriptors = $command.getProperty("Commands").getValue())
-#foreach( $cd in $commandDescriptors )
-        add$cd.getOrder()(#new_command_instantiation_nt($cd.getName(), $cd.getParameters().getValue()));
-#end
