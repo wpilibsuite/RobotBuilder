75811bf172df98d61d05cdf2a5c7279d922c157a
Joe <sciencewhiz@users.noreply.github.com>
2020-07-28 20:33:26 -0700

    Fix SequentialCommandGroup
    Fixes #239

--- src/main/resources/export/java/SequentialCommandGroup-constructors.java
+++ src/main/resources/export/java/SequentialCommandGroup-constructors.java
@@ -4,8 +4,8 @@
 #set($last = $len + 1)
 
 #if( $params.size() > 0 )
-    public #class($command.name)(#if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_java($params.get($i)), #end#end#param_declaration_java($params.get($last))) {
+    public #class($command.name)(#if( $len >= 0 )#foreach($i in [0..$len])#param_declaration_java($params.get($i)), #end#end#param_declaration_java($params.get($last)), #if ($command.getProperty("Requires").getValue() != "None")#class($command.getProperty("Requires").getValue()) #variable($command.getProperty("Requires").getValue())#end) {
 
 #else
-    public #class($command.name)() {
+    public #class($command.name)(#if ($command.getProperty("Requires").getValue() != "None")#class($command.getProperty("Requires").getValue()) #variable($command.getProperty("Requires").getValue())#end){
 #end
