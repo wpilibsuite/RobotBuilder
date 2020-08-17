1dde163bec1ed2c6ddb1dba52def72befdd98ed5
Joe <sciencewhiz@users.noreply.github.com>
2020-08-16 19:50:18 -0700

    fix default commands

--- src/main/resources/export/cpp/Subsystem-default_command.cpp
+++ src/main/resources/export/cpp/Subsystem-default_command.cpp
@@ -1,9 +1,14 @@
-#set($subsystem = $helper.getByName($subsystem_name, $robot))
-#set($command = $subsystem.getProperty("Default Command").getValue())
-#set($params = $subsystem.getProperty("Default command parameters").getValue())
+#foreach ($component in $components)
+#if ("#type($component)" == "frc2::SubsystemBase" || "#type($component)" == "frc2::PIDSubsystem")
+#foreach ($command in $commands)
+#if($command.name == $component.getProperty("Default Command").value)
 #set($len = $params.size() - 2)
 #set($last = $len + 1)
 
 #if ($command != "None")
-        SetDefaultCommand(#command_instantiation( $command $params ));
+        m_#required_subsystem($command)->SetDefaultCommand(#class($command.name)(m_#required_subsystem($command)));
+#end
+#end
+#end
+#end
 #end
