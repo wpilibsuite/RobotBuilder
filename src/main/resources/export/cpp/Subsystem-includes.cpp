885d483662dc73ab1add7ce4cd5af3d9c6a8ee63
Joe <sciencewhiz@users.noreply.github.com>
2020-10-03 19:53:16 -0700

    Fix subystem includes

--- src/main/resources/export/cpp/Subsystem-includes.cpp
+++ src/main/resources/export/cpp/Subsystem-includes.cpp
@@ -1,4 +1,1 @@
-#set($subsystem = $helper.getByName($subsystem_name, $robot))
-#set($default_command = $subsystem.getProperty("Default Command").getValue())
-#if($default_command != "None")\#include "Commands/#class($default_command).h"
-#end
+\#include "Subsystems/#class($subsystem.name).h"
