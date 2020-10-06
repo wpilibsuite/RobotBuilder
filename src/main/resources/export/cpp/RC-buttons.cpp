6609ff957d428575070588b96224358be9d24019
Joe <sciencewhiz@users.noreply.github.com>
2020-10-05 19:09:27 -0700

    Fix export of commands without required subystems

--- src/main/resources/export/cpp/RC-buttons.cpp
+++ src/main/resources/export/cpp/RC-buttons.cpp
@@ -4,6 +4,7 @@
 #foreach ($command in $commands)
 #if($command.name == $component.getProperty("Command").value)
 #constructor($component)
+
 #end
 #end
 #end
@@ -13,7 +14,8 @@
 #if ($helper.exportsTo("OI", $component) && ("#type($component)" != "Joystick") && ("#constructor($component)" != "" || "#extra($component)" != ""))
 #foreach ($command in $commands)
 #if($command.name == $component.getProperty("Command").value)
-m_#variable($component.name).$component.getProperty("When to Run").getValue().substring(0,1).toUpperCase()$component.getProperty("When to Run").getValue().substring(1)(#class($command.name)(&m_#required_subsystem($command))#if($command.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value)#end);#else#if($command.getProperty("Add Timeout").value == true).withTimeout($command.getProperty("Timeout").value));#end#end
+m_#variable($component.name).$component.getProperty("When to Run").getValue().substring(0,1).toUpperCase()$component.getProperty("When to Run").getValue().substring(1)(#class($command.name)(#if($command.getProperty("Requires").getValue() != "None")&m_#required_subsystem($command)#end)#if($command.getProperty("Add Timeout").value == true).withTimeout($component.getProperty("Timeout").value)#end);#else#if($command.getProperty("Add Timeout").value == true).withTimeout($command.getProperty("Timeout").value));#end#end
+
 #end
 #end
 #end
