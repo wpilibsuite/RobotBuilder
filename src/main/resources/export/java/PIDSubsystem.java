135e0becd5e3757b829c7fd5a540b36335f73e82
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-11-06 19:38:38 -0800

    Fix PIDSubsystem not running PIDController (#295)
    PIDSubsystem periodic wasn't being called. Accidentally was called in
    subsystem instead.

--- src/main/resources/export/java/PIDSubsystem.java
+++ src/main/resources/export/java/PIDSubsystem.java
@@ -45,6 +45,7 @@ public class #class($subsystem.name) extends PIDSubsystem {
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+        super.periodic();
 
     }
 
