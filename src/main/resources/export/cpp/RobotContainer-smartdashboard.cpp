3c9e3da001e086f4735d9e936c581101c737a19b
Joe <sciencewhiz@users.noreply.github.com>
2020-11-16 19:57:43 -0800

    Add missing file

---
+++ src/main/resources/export/cpp/RobotContainer-smartdashboard.cpp
@@ -0,0 +1,23 @@
+    // Smartdashboard Subsystems
+#foreach ($component in $components)
+#if ($helper.exportsTo("RobotContainer", $component))
+#if ($component.getProperty("Send to SmartDashboard").getValue())
+    frc::SmartDashboard::PutData(&m_#variable($component.name));
+#end
+#end
+#end
+
+${Collections.reverse($components)}
+    // SmartDashboard Buttons
+#foreach( $component in $components )
+#if ($component.getBase().getType() == "Command"
+     && $component.getProperty("Button on SmartDashboard").getValue())
+#if( $component.getProperty("Parameter presets").getValue().isEmpty())
+    frc::SmartDashboard::PutData("$component.getName()", new #new_command_instantiation( $component, $component, $set.getParameters()));
+#else
+#foreach( $set in $component.getProperty("Parameter presets").getValue() )
+    frc::SmartDashboard::PutData("$component.getName(): $set.getName()", new #new_command_instantiation( $component, $component, $set.getParameters() ));
+#end
+#end
+#end
+#end
