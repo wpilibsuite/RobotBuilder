9eb19e2486f5f7aa0dee526cba408fc07327c715
sciencewhiz <sciencewhiz@users.noreply.github.com>
2020-12-10 22:57:26 -0800

    Update to export to Beta 4 (#328)

--- src/main/java/robotbuilder/exporters/GenericExporter.java
+++ src/main/java/robotbuilder/exporters/GenericExporter.java
@@ -158,7 +158,7 @@ public class GenericExporter {
             }
         }
         if(wpilibRelease == null) {
-            wpilibRelease = "2021.1.1-beta-2"; // this shouldn't need to be relied upon,
+            wpilibRelease = "2021.1.1-beta-4"; // this shouldn't need to be relied upon,
                                                // but its better than generating nothing.
         }
 
