0a7e9904e61bb6416beb709bf379379c08c61bec
Joe <sciencewhiz@users.noreply.github.com>
2020-12-18 21:06:21 -0800

    Implement C++ XBox Controller support

--- src/main/resources/export/cpp/RobotContainer-constructors.cpp
+++ src/main/resources/export/cpp/RobotContainer-constructors.cpp
@@ -17,7 +17,7 @@ m_setpoint,
 ##${Collections.reverse($components)}
 #foreach( $component in $components )
 #if ($helper.exportsTo("OI", $component)
-     && ("#constructor($component)" != "" || "#extra($component)" != "") && "#type($component)" != "Joystick" && "#type($component)" != "JoystickButton")
+     && ("#constructor($component)" != "" || "#extra($component)" != "") && "#base_type($component)" != "Joystick" && "#type($component)" != "JoystickButton")
     #constructor($component)
     #extra($component)
 #end
