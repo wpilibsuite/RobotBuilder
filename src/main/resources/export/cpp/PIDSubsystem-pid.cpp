2befdae54d4de1e1fed500f35bfb3f0042f88e08
sciencewhiz <sciencewhiz@users.noreply.github.com>
2021-01-17 14:13:57 -0800

    Set Subsystem name for C++ (#356)
    Allows LiveWindow items to be grouped by subsystem

--- src/main/resources/export/cpp/PIDSubsystem-pid.cpp
+++ src/main/resources/export/cpp/PIDSubsystem-pid.cpp
@@ -2,6 +2,7 @@
 #class($subsystem.name)::#class($subsystem.name)() : frc2::PIDSubsystem(frc2::PIDController{kP, kI, kD}) {
     m_controller.SetTolerance(${subsystem.getProperty("Tolerance").getValue()});
     SetName("$subsystem_name");
+    SetSubsystem("$subsystem.name");
 #if($subsystem.getProperty("Limit Input").getValue())
     GetController().EnableContinuousInput(${subsystem.getProperty("Minimum Input").getValue()}, ${subsystem.getProperty("Maximum Input").getValue()});
 #end
