290357c867afb0e458ce745b85f333d8150c6282
sciencewhiz <sciencewhiz@users.noreply.github.com>
2021-01-29 20:42:46 -0800

    Make subsystems public in RobotContainer (#370)
    Allows commands that don't require the subystem to access them (Fixes
     #367), allows subystems to require multiple subsystems manually and
     avoids huge dependency chains for CommandGroups.

--- src/main/resources/export/cpp/RobotContainer-declarations.h
+++ src/main/resources/export/cpp/RobotContainer-declarations.h
@@ -1,12 +1,3 @@
-// The robot's subsystems
-#foreach ($component in $components)
-#if ("#type($component)" != "" 
-&& ("#type($component)" == "frc2::SubsystemBase"
-     || "#type($component)" == "frc2::PIDSubsystem"))
-    #class($component.getName()) #variable($component.getName());
-#end
-#end
-
 // Joysticks
 #foreach ($component in $components)
 #if ($helper.exportsTo("OI", $component)
