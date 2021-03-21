3fea546ac76354640574c297a0dd97b35842f612
Joe <sciencewhiz@users.noreply.github.com>
2021-03-20 20:11:00 -0700

    Add tests for type detection

--- src/test/java/robotbuilder/exporters/CppExportTest.java
+++ src/test/java/robotbuilder/exporters/CppExportTest.java
@@ -7,6 +7,7 @@ import robotbuilder.data.RobotComponent;
 import robotbuilder.data.RobotWalker;
 import robotbuilder.extensions.Extensions;
 import robotbuilder.robottree.RobotTree;
+import robotbuilder.utils.CodeFileUtils;
 
 import java.io.File;
 import java.io.IOException;
@@ -67,5 +68,47 @@ public class CppExportTest {
         exporter.export(tree);
 
         assertEquals("Exit value should be 0, compilation failed.", 0, TestUtils.runBuild(PROJECT_DIRECTORY));
+    }
+
+    @Test
+    public void testTypeDetection() throws IOException, InterruptedException {
+        String path = "build/test-resources/"+PROJECT_DIRECTORY+"/src/main/";
+        RobotTree tree = TestUtils.generateTestTree();
+        tree.getRoot().setName(PROJECT_DIRECTORY);
+        tree.getRoot().getProperty("Export Directory").setValueAndUpdate(new File("build/test-resources/").getAbsolutePath());
+        tree.walk(new RobotWalker() {
+            @Override
+            public void handleRobotComponent(RobotComponent self) { // Gives us better diagnostics when the robot tree isn't valid.
+                assertTrue("Component not valid: " + self + ": " + self.getErrorMessage(), self.isValid());
+            }
+        });
+        assertTrue("Robot tree is not valid.", tree.isRobotValid()); // Fails early instead of opening up a window to report failure.
+        GenericExporter exporter = new GenericExporter("/export/cpp/");
+        exporter.post_export_action = null;
+        exporter.export(tree);
+
+        //Command
+        assertEquals("CommandHelper", CodeFileUtils.getSavedSuperclass(new File(path + "include/commands/ArmDown.h")));
+        assertEquals("CommandHelper", CodeFileUtils.getSavedSuperclass(new File(path + "cpp/commands/ArmDown.cpp")));
+
+        //Instant Command
+        assertEquals("InstantCommand", CodeFileUtils.getSavedSuperclass(new File(path + "include/commands/InstantCommand1.h")));
+        assertEquals("InstantCommand", CodeFileUtils.getSavedSuperclass(new File(path + "cpp/commands/InstantCommand1.cpp")));
+
+        //Setpoint Command
+        assertEquals("CommandHelper", CodeFileUtils.getSavedSuperclass(new File(path + "include/commands/WristSetpoint.h")));
+        assertEquals("CommandHelper", CodeFileUtils.getSavedSuperclass(new File(path + "cpp/commands/WristSetpoint.cpp")));
+
+        //Sequential Command Group
+        assertEquals("CommandHelper", CodeFileUtils.getSavedSuperclass(new File(path + "include/commands/WristSetpoint.h")));
+        assertEquals("CommandHelper", CodeFileUtils.getSavedSuperclass(new File(path + "cpp/commands/WristSetpoint.cpp")));
+
+        //Subsystem
+        assertEquals("SubsystemBase", CodeFileUtils.getSavedSuperclass(new File(path + "include/subsystems/Arm.h")));
+        assertEquals("SubsystemBase", CodeFileUtils.getSavedSuperclass(new File(path + "cpp/subsystems/Arm.cpp")));
+
+        //PID Subsystem
+        assertEquals("PIDSubsystem", CodeFileUtils.getSavedSuperclass(new File(path + "include/subsystems/Wrist.h")));
+        assertEquals("PIDSubsystem", CodeFileUtils.getSavedSuperclass(new File(path + "cpp/subsystems/Wrist.cpp")));
     }
 }
