2905bea467fbb2256dc006aad41aa06417f96e1a
Joe <sciencewhiz@users.noreply.github.com>
2021-06-10 21:36:03 -0700

    Fix C++ compile

--- src/main/resources/export/cpp/PIDSubsystem-output.cpp
+++ src/main/resources/export/cpp/PIDSubsystem-output.cpp
@@ -2,9 +2,9 @@
 #foreach ($component in $components)
 #if ($component.name == $subsystem.getProperty("Output").getValue())
     #if($subsystem.getProperty("Limit Output").getValue())
-    #variable($component.name).PIDWrite(std::clamp(output, ${subsystem.getProperty("Minimum Output").getValue()}, ${subsystem.getProperty("Maximum Output").getValue()}));
+    #variable($component.name).Set(std::clamp(output, ${subsystem.getProperty("Minimum Output").getValue()}, ${subsystem.getProperty("Maximum Output").getValue()}));
 #else
-    #variable($component.name).PIDWrite(output);
+    #variable($component.name).Set(output);
 #end
 #end
 #end
