06fdac903789d9f775a67eb14b8d143d51672767
sciencewhiz <sciencewhiz@users.noreply.github.com>
2021-11-08 21:29:47 -0800

    Update for 2022 beta 2 gradle changes (#416)
    Update Gradle 7.2

--- src/test/java/robotbuilder/exporters/JavaExportTest.java
+++ src/test/java/robotbuilder/exporters/JavaExportTest.java
@@ -45,6 +45,9 @@ public class JavaExportTest {
 
     @After
     public void tearDown() {
+        System.gc();
+        File project = new File("build/test-resources/" + PROJECT_DIRECTORY);
+        TestUtils.delete(project);
     }
 
     @Test
