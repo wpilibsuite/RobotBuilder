6a12a22e7d0b20f171eab824bdf7e9bb15e674ae
sciencewhiz <sciencewhiz@users.noreply.github.com>
2022-01-03 22:29:03 -0800

    Update to compile with 2022.1.1-rc-1

--- src/main/java/robotbuilder/exporters/GenericExporter.java
+++ src/main/java/robotbuilder/exporters/GenericExporter.java
@@ -158,8 +158,8 @@ public class GenericExporter {
             }
         }
         if(wpilibRelease == null) {
-            wpilibRelease = "2022.1.1-beta-4"; // this shouldn't need to be relied upon,
-                                               // but its better than generating nothing.
+            wpilibRelease = "2022.1.1-rc-1"; // this shouldn't need to be relied upon,
+                                             // but its better than generating nothing.
         }
 
         // Prepare the main context
