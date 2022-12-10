804ff95b269f6a8cbaad66f0930ce9088014e3ac
Peter Johnson <johnson.peter@gmail.com>
2022-12-09 23:13:14 -0800

    Update to export to 2023.1.1-beta-6 (#515)

--- src/main/java/robotbuilder/exporters/GenericExporter.java
+++ src/main/java/robotbuilder/exporters/GenericExporter.java
@@ -158,7 +158,7 @@ public class GenericExporter {
             }
         }
         if(wpilibRelease == null) {
-            wpilibRelease = "2023.1.1-beta-5"; // this shouldn't need to be relied upon,
+            wpilibRelease = "2023.1.1-beta-6"; // this shouldn't need to be relied upon,
                                                // but its better than generating nothing.
         }
 
