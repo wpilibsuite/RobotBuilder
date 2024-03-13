f9a56b29d7a9d79efcdb25a05ebb132e62dfbd92
sciencewhiz <sciencewhiz@users.noreply.github.com>
2024-03-13 06:44:24 -0700

    Update to export to 2024.3.2 (#600)

--- src/main/java/robotbuilder/exporters/GenericExporter.java
+++ src/main/java/robotbuilder/exporters/GenericExporter.java
@@ -158,7 +158,7 @@ public class GenericExporter {
             }
         }
         if(wpilibRelease == null) {
-            wpilibRelease = "2024.3.1"; // this shouldn't need to be relied upon,
+            wpilibRelease = "2024.3.2"; // this shouldn't need to be relied upon,
                                         // but its better than generating nothing.
         }
 
