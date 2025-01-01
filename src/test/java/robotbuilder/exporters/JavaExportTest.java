6ad4f4d65fe95cdedaae192b598f770a1204e84a
sciencewhiz <sciencewhiz@users.noreply.github.com>
2024-12-31 22:47:52 -0800

    Revert "Disable compile tests until GradleRIO released (#634)"
    This reverts commit 5d1459ee06498f4640098f20c485c4f812ff1c2f.

--- src/test/java/robotbuilder/exporters/JavaExportTest.java
+++ src/test/java/robotbuilder/exporters/JavaExportTest.java
@@ -52,7 +52,6 @@ public class JavaExportTest {
         TestUtils.delete(project);
     }
 
-    @Ignore("Disabling compile test until GradleRIO version released")
     @Test
     public void testJavaExport() throws IOException, InterruptedException {
         RobotTree tree = TestUtils.generateTestTree();
