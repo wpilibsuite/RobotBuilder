95b5bb491c83870c3cc13d9ee239b378f2e512b1
sciencewhiz <sciencewhiz@users.noreply.github.com>
2025-10-02 16:19:44 -0700

    Update projectyear to 2026beta

--- src/main/java/robotbuilder/RobotBuilder.java
+++ src/main/java/robotbuilder/RobotBuilder.java
@@ -25,7 +25,7 @@ public class RobotBuilder {
 
     public static final String VERSION = VERSION_MAJOR + "." + VERSION_MINOR;
 
-    public static final String WPILIB_VERSION = "2025";
+    public static final String WPILIB_VERSION = "2026";
 
     public static final String FRC_HOME = findFrcHome(WPILIB_VERSION);
 
